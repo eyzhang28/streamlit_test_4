ced064d4a57243db4d219fe690c3a70819884e13
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-10 21:29:54 -0400

    Update streamlit_test3.py

--- streamlit_test3.py
+++ streamlit_test3.py
@@ -169,6 +169,6 @@ if st.button("Run Script"):
      df3 = df2_transposed.reset_index(drop = True)
      df3.columns = df3.iloc[1]
      df3 = df3.drop([0,1])
-     df3 = df3.drop([21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79])
+     df3 = df3.drop([21, 22, 23, 24])
      st.write(df3)
      st.write(df3.columns)
