ced9a5fcb5e8a151997b9aebd12cc826335e1b00
Eric Zhang <51934960+eyzhang28@users.noreply.github.com>
2022-08-10 21:17:04 -0400

    Update streamlit_test3.py

--- streamlit_test3.py
+++ streamlit_test3.py
@@ -165,3 +165,4 @@ if st.button("Run Script"):
      st.write(df['Password'].iloc[0])
      df2 = pd.read_csv(state_data)
      df2 = df2.drop([2,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,43,44,45])
+     st.write(df2)
